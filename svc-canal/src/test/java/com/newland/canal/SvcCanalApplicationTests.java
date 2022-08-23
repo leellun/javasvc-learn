2e025444506d31569ca5184a5a765ca5125f293c
leellun <leellun@sina.cn>
2022-08-24 00:59:00 +0800

    canal/SimpleCanalClientExample.java

---
+++ svc-canal/src/test/java/com/newland/canal/SvcCanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newland.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcCanalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
