a4aabcbc040505feed84952987d2fd99941755f2
leellun <leellun@sina.cn>
2022-09-02 12:05:56 +0800

    aop

--- java-spring/src/com/newland/spring/aop/aspects/MathCalculator.java
+++ java-spring/src/com/newland/spring/aop/aspects/MathCalculator.java
@@ -7,6 +7,12 @@ package com.newland.spring.aop.aspects;
 public class MathCalculator {
     public int div(int i,int j){
         System.out.println("MathCalculator...div...");
+        int sum=add(i,j);
+        System.out.println(sum);
         return i/j;
+    }
+    public int add(int i,int j){
+        System.out.println("MathCalculator...div...");
+        return i+j;
     }
 }
